f3353c6d484165c078b3113cafc43018f583913c
John Freeman <jlfreeman96@gmail.com>
2023-07-04 12:15:47 -0400

    Three small changes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,16 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+	light: {
+		text: '#000',
+		background: '#fff',
+		tint: '#2f95dc',
+		tabIconDefault: '#ccc',
+		tabIconSelected: '#2f95dc',
+	},
+	dark: {
+		text: '#fff',
+		background: '#000',
+		tint: '#fff',
+		tabIconDefault: '#ccc',
+		tabIconSelected: '#fff',
+	},
 };
